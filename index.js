709de0a600b39e7205d21723b00b448175b4b39a
Murat Yildirim <myildirim.89@gmail.com>
2023-05-20 11:36:48 +0200

    caching on building image

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.json([{
-  name: 'Bob',
-  email: 'bob@gmail.com'
-}]))
+app.get('/', (req, res) => res.json([
+    {
+      name: 'Alice',
+      email: 'alice@gmail.com'
+    },
+    {
+      name: 'Jake',
+      email: 'jake@gmail.com'
+    },
+    {
+      name: 'James',
+      email: 'james@gmail.com'
+    },
+    {
+      name: 'Bob',
+      email: 'bob@gmail.com'
+    }
+]))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
